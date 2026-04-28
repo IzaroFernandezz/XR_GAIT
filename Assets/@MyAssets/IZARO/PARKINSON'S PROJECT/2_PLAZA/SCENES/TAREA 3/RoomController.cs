64713a1b2eabc76fe709a688d2f6ab409e3b4efe
IzaroFernandezz <fernandezizaro2004@gmail.com>
2026-04-28 17:21:19 +0200

    Cosas de Izaro + Centrar cosas al suelo

---
+++ Assets/@MyAssets/IZARO/PARKINSON'S PROJECT/2_PLAZA/SCENES/TAREA 3/RoomController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Meta.XR.MRUtilityKit;
+
+public class RoomController : MonoBehaviour
+{
+    [Header("Prefab que quieres colocar en el centro del suelo")]
+    public GameObject roomPrefab;
+
+    [Header("Altura extra sobre el suelo")]
+    public float yOffset = 0.02f;
+
+    [SerializeField] private GameObject spawnedRoom;
+
+    private void Start()
+    {
+        Invoke(nameof(PlaceRoomInFloorCenter), 0.5f);
+    }
+
+    public void PlaceRoomInFloorCenter()
+    {
+        MRUKRoom currentRoom = MRUK.Instance.GetCurrentRoom();
+
+        if (currentRoom == null)
+        {
+            Debug.LogWarning("No se ha encontrado ninguna habitación MRUK.");
+            return;
+        }
+
+        MRUKAnchor floorAnchor = currentRoom.FloorAnchor;
+
+        if (floorAnchor == null)
+        {
+            Debug.LogWarning("No se ha encontrado el suelo en MRUK.");
+            return;
+        }
+
+        Vector3 floorCenter = floorAnchor.transform.position;
+        floorCenter.y += yOffset;
+
+        if (spawnedRoom == null)
+        {
+            spawnedRoom = Instantiate(roomPrefab, floorCenter, floorAnchor.transform.rotation);
+        }
+        else
+        {
+            spawnedRoom.transform.position = floorCenter;
+            //spawnedRoom.transform.rotation = floorAnchor.transform.rotation;
+        }
+
+        Debug.Log("Room colocado en el centro del suelo.");
+    }
+}
